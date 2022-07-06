af9043047ef3c39725ee2e66dddc458033da05b6
Matt <mattschock392@gmail.com>
2022-07-06 14:45:26 -0500

    working

--- index.js
+++ index.js
@@ -8,31 +8,20 @@ class Cards {
 
 let currentDeck = []
 
-let value = 0
+// let value = 0
 
 function fillDeck() {
     let suit = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
     let numFace = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+    let value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     for (let i = 0; i < suit.length; i++) {
         for (let y = 0; y < numFace.length; y++) {
             
-            currentDeck.push(new Cards(suit[i], numFace[y], value))
+            currentDeck.push(new Cards(suit[i], numFace[y], value[y]))
         }
     }
 }
 fillDeck();
-
-// for (let i = 0; i < currentDeck.length; i++) {
-//     switch (currentDeck[i].numFace) {
-//         case 'Ace':
-//             value = 14
-//             break;
-//         default:
-//             value = 10
-        
-//     }
-
-// }
 
 function shuffle() {
     for(let i = currentDeck.length - 1; i >= 1; i--) {
@@ -56,13 +45,37 @@ while(currentDeck.length > 0) {
     player2Deck.push(currentDeck.splice(draw,1)[0]);
 }
 
-console.log(player1Deck);
-console.log(player2Deck);
+
 class playGame {
     constructor() {
-        this.player1Score = player1Score
-        this.player2Score = player2Score
+        this.player1Score = 0;
+        this.player2Score = 0;
+    }
+            //compare index0 from player arrays
+    
+    startGame() {
+        for(let i = 0; i < 26; i++) {
+            if(player1Deck[0].value > player2Deck[0].value) {
+                this.player1Score++;
+                console.log(`Player1 played ${player1Deck[0]}, player2 played ${player2Deck[0]} Player1 wins the hand. Current score player1= ${player1Score} Player2= ${player2Score}`);
+                player1Deck.shift();
+                player2Deck.shift();
+            } else if (player2Deck[0].value > player1Deck[0].value) {
+                this.player2Score++;
+                console.log(`Player1 played ${player1Deck[0]}, player2 played ${player2Deck[0]} Player2 wins the hand. Current score player1= ${player1Score} Player2= ${player2Score}`);
+                player1Deck.shift();
+                player2Deck.shift();
+            } else {
+                console.log(`Player1 played ${player1Deck[0]}, player2 played ${player2Deck[0]}. Tie!  Current score remains player1= ${player1Score}, player2= ${player2Score}`);
+                player1Deck.shift();
+                player2Deck.shift();
+            }
+        }
     }
 
-    
 }
+let warGame = new playGame();
+warGame.startGame();
+
+
+
