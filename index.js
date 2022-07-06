6e7deeae65ef35dbe1deee2a7f4647b7f6e7b9c1
Matt <mattschock392@gmail.com>
2022-07-06 13:50:43 -0500

    update

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 class Cards {
-    constructor(suit, numFace) {
+    constructor(suit, numFace, value) {
         this.suit = suit;
         this.numFace = numFace;
+        this.value = value;
     }
 }
 
 let currentDeck = []
+
+let value = 0
 
 function fillDeck() {
     let suit = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
     let numFace = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
     for (let i = 0; i < suit.length; i++) {
         for (let y = 0; y < numFace.length; y++) {
-            currentDeck.push(new Cards(suit[i], numFace[y]))
+            
+            currentDeck.push(new Cards(suit[i], numFace[y], value))
         }
     }
 }
 fillDeck();
+
+// for (let i = 0; i < currentDeck.length; i++) {
+//     switch (currentDeck[i].numFace) {
+//         case 'Ace':
+//             value = 14
+//             break;
+//         default:
+//             value = 10
+        
+//     }
+
+// }
 
 function shuffle() {
     for(let i = currentDeck.length - 1; i >= 1; i--) {
@@ -40,10 +56,13 @@ while(currentDeck.length > 0) {
     player2Deck.push(currentDeck.splice(draw,1)[0]);
 }
 
+console.log(player1Deck);
+console.log(player2Deck);
 class playGame {
     constructor() {
         this.player1Score = player1Score
         this.player2Score = player2Score
     }
+
     
 }
